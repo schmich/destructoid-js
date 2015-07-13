52e1b375314fbdc2cac614fcfbe69cab3f4426b8
Chris Schmich <schmch@gmail.com>
2015-07-12 21:36:33 -0500

    Move core plugin loading into bot.js. Expose `on` method for raw events.

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var EventEmitter = require('events').EventEmitter,
     irc = require('irc'),
     async = require('asyncawait/async'),
-    async = require('asyncawait/await'),
+    await = require('asyncawait/await'),
     Log = require('./log'),
     sprintf = require('sprintf'),
     Promise = require('bluebird');
@@ -30,19 +30,87 @@ function Bot(opts) {
     return this;
   };
 
-  function loadPlugin(plugin) {
-    plugin.load(self.emitter);
+  function makeChannel(client, channelName) {
+    return {
+      name: channelName,
+      say: function() {
+        client.say('#' + channelName, sprintf.apply(null, arguments));
+      },
+      ban: function(user) {
+        // TODO: Canonicalize username.
+        this.say('.ban ' + user);
+      },
+      unban: function(user) {
+        // TODO: Canonicalize username.
+        this.say('.unban ' + user);
+      }
+    };
+  }
+
+  function makeUser(client, username) {
+    function channelSay(channel, message) {
+      client.say('#' + channelName, sprintf.apply(null, arguments));
+    }
+
+    // TODO: Canonicalize username.
+    return {
+      name: username,
+      whisper: function() {
+        throw new Error('user.whisper');
+      },
+      ban: function(channel) {
+        channelSay(channel, '.ban ' + username);
+      },
+      unban: function(channel) {
+        channelSay(channel, '.unban ' + username);
+      }
+    };
+  }
+
+  this.loadPlugin = function(plugin) {
+    if (plugin.load) {
+      plugin.load(emitter);
+    }
+
+    if (plugin.message) {
+      this.on('message', async(function(client, channel, user, message) {
+        plugin.message(makeChannel(client, channel), makeUser(client, user), message);
+      }));
+    }
+
+    if (plugin.join) {
+      this.on('join', async(function(client, channel, user) {
+        plugin.join(makeChannel(client, channel), makeUser(client, user));
+      }));
+    }
+
+    if (plugin.part) {
+      this.on('part', async(function(client, channel, user) {
+        plugin.part(makeChannel(client, channel), makeUser(client, user));
+      }));
+    }
+
+    if (plugin.channel) {
+      this.on('channel', async(function(client, channel) {
+        plugin.channel(makeChannel(client, channel));
+      }));
+    }
   }
 
   this.plugin = function(plugin) {
     if (plugin instanceof Array) {
       for (var i = 0; i < plugin.length; ++i) {
-        loadPlugin(plugin[i]);
+        this.loadPlugin(plugin[i]);
       }
     } else {
-      loadPlugin(plugin);
+      this.loadPlugin(plugin);
     }
 
+    return this;
+  };
+
+  this.on = function() {
+    this.emitter.on.apply(this.emitter, arguments);
     return this;
   };
 
@@ -72,10 +140,6 @@ function Bot(opts) {
     this.client.promise.then(function(client) {
       client.on('message#', function(user, channel, message) {
         user = user.trim().toLowerCase();
-        if (user === 'jtv') {
-          return;
-        }
-
         channel = channel.substr(1).trim().toLowerCase();
 
         self.emitter.emit('message', client, channel, user, message);
@@ -83,10 +147,6 @@ function Bot(opts) {
 
       client.on('join', function(channel, user) {
         user = user.trim().toLowerCase();
-        if (user === 'jtv') {
-          return;
-        }
-
         channel = channel.substr(1).trim().toLowerCase();
 
         self.emitter.emit('join', client, channel, user);
@@ -94,10 +154,6 @@ function Bot(opts) {
 
       client.on('part', function(channel, user) {
         user = user.trim().toLowerCase();
-        if (user === 'jtv') {
-          return;
-        }
-
         channel = channel.substr(1).trim().toLowerCase();
 
         self.emitter.emit('part', client, channel, user);
